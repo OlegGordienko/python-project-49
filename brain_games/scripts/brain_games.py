5c767e57ea3cfabe21a37856136d76ed027df62a
Oleg Gordienko <oleg208625@gmail.com>
2024-10-29 12:14:01 +0600

    Add welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+def say_welcome():
+	print("Welcome to the Brain Games!")
 
 def main():
-	print("Welcome to the Brain Games!")
+	say_welcome()
+	welcome_user()
 
 if __name__ == '__main__':
 	main()
